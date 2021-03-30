edf44412f923ee2069e385ad48f35385e0f17ba3
Klara Hajdu <hajdu.klara.78@gmail.com>
2021-03-31 00:06:33 +0200

    feat: add firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.localhost';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'collabox-angular';
+
+
+  constructor () {
+
+    console.log();
+    console.log(environment.firebaseConfig)
+  }
+
 }
