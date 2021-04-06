3211dfa2b9a7fcd698c287c643bcb57b75d400a2
Klara Hajdu <hajdu.klara.78@gmail.com>
2021-04-06 16:31:14 +0200

    feat: add angular material and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularMaterialModule } from './angular-material.module';
 import { environment } from '../environments/environment';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
+import { AddPlaylistComponent } from './components/addplaylist/addplaylist.component';
 import { AuthService } from './services/auth-service.service';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     LandingComponent,
     LoginComponent,
     SidebarComponent
+    AddPlaylistComponent
   ],
   imports: [
     BrowserModule,
@@ -25,8 +29,11 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [AuthService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/components/addplaylist/addplaylist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'addplaylist',
+  templateUrl: './addplaylist.component.html',
+  styleUrls: ['./addplaylist.component.sass']
+})
+export class AddPlaylistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
