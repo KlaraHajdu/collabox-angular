f1a917c25256a0b5ca9a22b00b29a75e4389b553
Klara Hajdu <hajdu.klara.78@gmail.com>
2021-04-14 11:28:21 +0200

    feat: play songs

--- src/app/components/playsong/playsong.component.ts
+++ src/app/components/playsong/playsong.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { select } from '@angular-redux/store';
 import { Observable } from 'rxjs';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import RootState from 'src/app/store/RootState';
 import Song from 'src/app/types/Song';
 
@@ -12,12 +12,12 @@ import Song from 'src/app/types/Song';
 })
 export class PlaysongComponent implements OnInit {
   @ViewChild('player') player: any;
-  @select((state: RootState) => state.playlists.currentPlaylist?.songs) songs$: Observable<any[]>;
-  notPlayedSongs$: Observable<any[]>;
-  notPlayed: any[];
+  @select((state: RootState) => state.playlists.currentPlaylist?.songs)
+  songs$: Observable<any[]>;
   songsLength: number;
   currentSong: Pick<Song, 'youtubeId' | 'title'> | undefined;
   playedSongs: Pick<Song, 'youtubeId' | 'title'>[];
+  playStopped: boolean;
   currentSongForwardIndex: number;
   currentSongBackwardIndex: number;
   canSkipForward: boolean;
@@ -39,13 +39,11 @@ export class PlaysongComponent implements OnInit {
     this.isMuted = false;
 
     this.songs$.subscribe((songs) => {
-      this.songsLength = songs.length
+      this.songsLength = songs.length;
       if (this.playedSongs.length === 0) {
         this.currentSong = songs[0];
       }
     });
-
-
   }
 
   onReady(event: any) {
@@ -54,16 +52,25 @@ export class PlaysongComponent implements OnInit {
   }
 
   startPlay() {
+    this.playStopped = false;
     this.player.playVideo();
   }
 
   onStateChange(event: any) {
-    if (event.data === window['YT'].PlayerState.ENDED) {
-      this.skipToNext()
+    switch (event.data) {
+      case window['YT'].PlayerState.ENDED:
+        this.skipToNext();
+        break;
+      case window['YT'].PlayerState.CUED:
+        if (!this.playStopped) {
+          this.player.playVideo();
+        }
+        break;
     }
   }
 
   stopPlay() {
+    this.playStopped = true;
     this.player.stopVideo();
   }
 
@@ -72,48 +79,57 @@ export class PlaysongComponent implements OnInit {
   }
 
   changeCurrentSong() {
-    if (this.songs$ ) {
+    if (this.songs$) {
       if (this.currentSongBackwardIndex > 0) {
-        this.currentSong = this.playedSongs[this.playedSongs.length - this.currentSongBackwardIndex];
-    } else {
-      const playedSongsYoutubeIds = this.playedSongs.map(
-        (playedSong: Pick<Song, 'youtubeId' | 'title'>) =>
-          playedSong.youtubeId
-      );
+        this.currentSong = this.playedSongs[
+          this.playedSongs.length - this.currentSongBackwardIndex
+        ];
+      } else {
+        const playedSongsYoutubeIds = this.playedSongs.map(
+          (playedSong: Pick<Song, 'youtubeId' | 'title'>) =>
+            playedSong.youtubeId
+        );
 
-      this.songs$
-          .pipe(map(songs => songs.filter(s => {
-            console.log(s)
-            return !playedSongsYoutubeIds.includes(s.youtubeId)
-          }))).subscribe((res) => {
-            console.log(res)
-            this.currentSong = res[0]
-            })
+        this.songs$
+          .pipe(
+            map((songs) =>
+              songs.filter((s) => {
+                return !playedSongsYoutubeIds.includes(s.youtubeId);
+              })
+            )
+          )
+          .subscribe((notPlayedSongs) => {
+            this.currentSong = notPlayedSongs[0];
+          });
+      }
     }
-    setTimeout(() => this.startPlay(), 500)
-  }
   }
 
   skipToNext() {
-
-    if (this.currentSongBackwardIndex === 0 && this.currentSongForwardIndex < this.songsLength - 1) {
+    if (
+      this.currentSongBackwardIndex === 0 &&
+      this.currentSongForwardIndex < this.songsLength - 1
+    ) {
       this.playedSongs = [...this.playedSongs, this.currentSong];
       this.currentSongForwardIndex += 1;
     } else if (this.currentSongBackwardIndex > 0) {
       this.currentSongBackwardIndex -= 1;
     }
 
-    if (this.currentSongBackwardIndex === 0 && this.currentSongForwardIndex === this.songsLength - 1) {
-        this.canSkipForward = false;
-      }
+    if (
+      this.currentSongBackwardIndex === 0 &&
+      this.currentSongForwardIndex === this.songsLength - 1
+    ) {
+      this.canSkipForward = false;
+    }
 
-      this.canSkipBackward = true;
-      this.changeCurrentSong();
-};
+    this.canSkipBackward = true;
+    this.changeCurrentSong();
+  }
 
   skipToPrevious = () => {
     if (this.currentSongBackwardIndex < this.playedSongs.length) {
-      this.currentSongBackwardIndex += 1 ;
+      this.currentSongBackwardIndex += 1;
     }
     if (this.currentSongBackwardIndex === this.playedSongs.length) {
       this.canSkipBackward = false;
@@ -124,11 +140,11 @@ export class PlaysongComponent implements OnInit {
 
   toggleMute = () => {
     if (!this.isMuted) {
-        this.player.mute()
-      }
-      if (this.isMuted) {
-        this.player.unMute()
-      }
-      this.isMuted = !this.isMuted;
-};
+      this.player.mute();
+    }
+    if (this.isMuted) {
+      this.player.unMute();
+    }
+    this.isMuted = !this.isMuted;
+  };
 }
