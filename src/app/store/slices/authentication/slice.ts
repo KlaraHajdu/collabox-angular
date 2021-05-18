6d94e3866d01bf8067b784af8f92bab822d9ddec
Klara Hajdu <hajdu.klara.78@gmail.com>
2021-05-18 14:31:04 +0200

    fix: delete unused function in auth slice

--- src/app/store/slices/authentication/slice.ts
+++ src/app/store/slices/authentication/slice.ts
@@ -1,37 +1,12 @@
-import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit'
-import firebase from 'firebase/app'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import User from '../../../types/User'
 import AuthState from './types/AuthState'
-// import { subscribeToAuthentication } from '../../../service/authentication'
 
 const initialState: AuthState = {
     currentUser: null,
     errorMessage: null,
     loading: true,
 }
-
-const subscribeToAuthChanges = createAsyncThunk(
-    'authentication/subscribeToAuthChanges',
-    async (payload, thunkApi) => {
-        const observer = (user: firebase.User) => {
-            if (user) {
-                const newUserState: User = {
-                    name: user.displayName || 'anonymous',
-                    email: user.email || 'no_email',
-                    id: user.uid,
-                }
-                thunkApi.dispatch(slice.actions.SET_CURRENT_USER(newUserState))
-            } else {
-                thunkApi.dispatch(slice.actions.SET_CURRENT_USER(null))
-            }
-        }
-        try {
-            // await subscribeToAuthentication(observer)
-        } catch (error) {
-            thunkApi.rejectWithValue('authentication_error')
-        }
-    }
-)
 
 const slice = createSlice({
     name: 'authentication',
@@ -48,4 +23,3 @@ export default slice.reducer
 
 export const authenticationActions = slice.actions
 
-export const authenticationAsyncActions = { subscribeToAuthChanges }
