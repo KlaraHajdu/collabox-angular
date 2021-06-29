dd02db360e6c5d7fbfe37d6ea1d179f5f8040e72
Klara Hajdu <hajdu.klara.78@gmail.com>
2021-06-29 22:05:34 +0200

    feat: add test to songlist

--- src/app/components/songlist/songlist.component.spec.ts
+++ src/app/components/songlist/songlist.component.spec.ts
@@ -1,4 +1,9 @@
+import { MockNgRedux } from '@angular-redux/store/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
+import RootState from 'src/app/store/RootState';
+import Song from 'src/app/types/Song';
+import User from 'src/app/types/User';
 
 import { SonglistComponent } from './songlist.component';
 
@@ -8,9 +13,44 @@ describe('SonglistComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SonglistComponent ]
-    })
-    .compileComponents();
+      declarations: [SonglistComponent],
+    }).compileComponents();
+    MockNgRedux.reset();
+
+    const songListStub: Subject<Song[]> =
+      MockNgRedux.getSelectorStub<RootState, Song[]>('songs');
+    const ownerIdStub: Subject<string> =
+      MockNgRedux.getSelectorStub<RootState, string>('owner');
+    const lockStatusStub: Subject<boolean> =
+      MockNgRedux.getSelectorStub<RootState, boolean>('lockStatus');
+    const currentUserStub: Subject<User> =
+      MockNgRedux.getSelectorStub<RootState, User>('currentUser');
+
+    songListStub.next([
+      {
+        id: 'fake_id',
+        youtubeId: 'fake_youtube_id',
+        title: 'fake_title',
+        votes: 8,
+        userId: 'fake_user_id',
+        userName: 'User',
+        upVoted: false,
+        downVoted: true,
+      },
+    ]);
+
+    ownerIdStub.next('fake_user_id');
+    lockStatusStub.next(false);
+    currentUserStub.next({
+      id: 'fake_user_id',
+      name: 'Fake user name',
+      email: 'fake_email',
+    });
+
+    songListStub.complete();
+    ownerIdStub.complete();
+    lockStatusStub.complete();
+    currentUserStub.complete();
   });
 
   beforeEach(() => {
