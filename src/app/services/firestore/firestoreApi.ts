d8aa3c0f6d09cf0e106906335a3d63ed672a4eb4
Klara Hajdu <hajdu.klara.78@gmail.com>
2021-06-30 19:24:56 +0200

    fix: optimise import

--- src/app/services/firestore/firestoreApi.ts
+++ src/app/services/firestore/firestoreApi.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import 'firebase/firestore'
 import PlaylistData from '../../types/PlaylistData';
 import PlaylistType from '../../types/PlaylistType';
