0e528e4ebd16c1998e0edc7b88d50082fab45aa9
Klara Hajdu <hajdu.klara.78@gmail.com>
2021-06-30 20:51:09 +0200

    feat: add google analytics

--- src/app/services/firestore/database.ts
+++ src/app/services/firestore/database.ts
@@ -1,6 +1,7 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/analytics';
 import { environment } from '../../../environments/environment';
 
 const firebaseApp = firebase.initializeApp(environment.firebaseConfig)
@@ -8,6 +9,15 @@ const firebaseApp = firebase.initializeApp(environment.firebaseConfig)
 const database = firebaseApp.firestore()
 const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
+
+const checkIfAnalyticsIsSupported = async () => {
+  const isSupported = await firebase.analytics.isSupported()
+  if (isSupported) {
+      firebase.analytics();
+  }
+}
+
+checkIfAnalyticsIsSupported()
 
 export { auth, provider }
 
